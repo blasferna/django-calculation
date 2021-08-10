470e62c38d150f33df3c1592be66945a3e3ed3c0
Blas Isaias Fernandez <blasferna@gmail.com>
2021-08-10 12:42:22 -0400

    setup.py improvements

--- setup.py
+++ setup.py
@@ -1,3 +1,35 @@
+#!/usr/bin/env python
 from setuptools import setup
 
-setup()
+setup(
+    name="django-calculation",
+    packages=["calculation"],
+    include_package_data=True,
+    version="0.0.1",
+    description="A Django app to make simple calculations in your django forms",
+    long_description=open("README.md").read(),
+	long_description_content_type="text/markdown",
+    author="Blas Fernandez",
+    author_email="blasferna@gmail.com",
+    url="https://github.com/blasferna/django-calculation",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+		"Framework :: Django :: 3.3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=["Django>=2.2"],
+    python_requires=">=3.6",
+)
